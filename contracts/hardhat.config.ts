76155b68b94ca69e265a98133252f7e7e806ea0a
Maciej Kożuszek <mackozuszek@gmail.com>
2024-01-18 00:05:08 +0100

    Deployment script for local node

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -4,9 +4,25 @@ import "@nomicfoundation/hardhat-foundry";
 import "@typechain/hardhat";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers";
+import "dotenv/config";
+
+const { PRIVATE_KEY } = process.env;
+
+console.log(PRIVATE_KEY);
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      accounts: [
+        {
+          privateKey: PRIVATE_KEY as string,
+          balance: "100000000000000000",
+        },
+      ],
+    },
+  },
 };
 
 export default config;
