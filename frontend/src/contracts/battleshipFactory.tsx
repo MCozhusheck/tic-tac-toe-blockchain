54ac9984e99b8e7e48d5bdb46ee81b70c43553dd
Maciej Kożuszek <mackozuszek@gmail.com>
2024-05-12 22:00:22 +0200

    Use local storage for storing private board

--- frontend/src/contracts/battleshipFactory.tsx
+++ frontend/src/contracts/battleshipFactory.tsx
@@ -2,7 +2,7 @@
 
 import { useAccount, useReadContract, useWriteContract } from "wagmi";
 import sepoliaDeploy from "../../../contracts/deployed-contracts/sepolia.json";
-import { abi } from "../../../contracts/artifacts/src/utils/BattleshipFactory.sol/BattleshipFactory.json";
+import BattleshipFactory from "../../../contracts/artifacts/src/utils/BattleshipFactory.sol/BattleshipFactory.json";
 
 export const useDeployBoard = () => {
   const { data: hash, isPending, error, writeContract } = useWriteContract();
@@ -10,7 +10,7 @@ export const useDeployBoard = () => {
   const deploy = async (playerBoardRootHash: string, stakeAmount: bigint) =>
     writeContract({
       address: sepoliaDeploy.battleshipFactory as `0x${string}`,
-      abi,
+      abi: BattleshipFactory.abi,
       functionName: "createBattleship",
       args: [playerBoardRootHash],
       value: stakeAmount,
@@ -22,7 +22,7 @@ export const useDeployBoard = () => {
 export const useGetDeployedBoards = () => {
   const { address } = useAccount();
   const { data, ...rest } = useReadContract({
-    abi,
+    abi: BattleshipFactory.abi,
     address: sepoliaDeploy.battleshipFactory as `0x${string}`,
     functionName: "getDeployedBattleships",
     args: [address],
